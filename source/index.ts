4e9d7d786800deabff1a453623969b751a00ed24
unknown <abdulameer444@gmail.com>
2022-08-25 14:04:23 +0500

    new

--- source/index.ts
+++ source/index.ts
@@ -1,41 +1,36 @@
-import express,{Request,Response} from 'express';
-import { json } from 'body-parser';
-import dotenv from 'dotenv';
-import rateLimit from  'express-rate-limit';
+import express, { Request, Response } from "express";
+import { json } from "body-parser";
+import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 
-import connectDB from './config/db';
+import connectDB from "./config/db";
 
+import dataRoute from "./routes/dataRoute";
 
-import dataRoute  from './routes/dataRoute'
-
-const app = express()
+const app = express();
 dotenv.config();
 connectDB();
-app.use(json())
+app.use(json());
 
 app.use(
-      rateLimit({
-       windowMs:1*60*1000, // 1 minutes
-       max:5, // Limit each IP to 5 requests per window
-       message: "You exceeded 5 requests in 1 minute limit!",
-       headers: true,
-})
-)
+  rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 5, // Limit each IP to 5 requests per window
+    message: "You exceeded 5 requests in 1 minute limit!",
+    headers: true,
+  })
+);
 
+app.get("/api/user", (req: Request, res: Response) => {
+  res.status(200).send("Hello");
+});
 
-app.get("/api/user",(req:Request,res:Response)=>{
-
-res.status(200).send("Hello")
-
-})
-
-app.use("",dataRoute);
+app.use("", dataRoute);
 
 //Port Listening Code Port Running on 5050
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
-
 
 module.exports = app.listen(3000);
--- source/tests/data.test.ts
+++ source/tests/data.test.ts
@@ -1,25 +1,23 @@
-
-
 // Do not use test DB because I am only fetching data from db
 // I used it if i need to update or delete data
 
+import request from "supertest";
+let app: any;
 
-
-import request from 'supertest';
-let app:any;
-
-
-  
-describe('/api/data',()=>{
-    beforeEach(()=> {
-
-      app=require('../index')}); 
-    afterEach(() =>{app.close();}) 
-describe('GET /',()=>{
-  jest.setTimeout(10*1000);
-  it('should return all data', async()=>{
-    const res = await request(app).get('/api/data?c=AT&dateFrom=2021-W05&dateTo=2021-W49&range=5');
-     expect(res.status).toBe(200)
+describe("/api/data", () => {
+  beforeEach(() => {
+    app = require("../index");
   });
-});
+  afterEach(() => {
+    app.close();
+  });
+  describe("GET /", () => {
+    jest.setTimeout(10 * 1000);
+    it("should return all data", async () => {
+      const res = await request(app).get(
+        "/api/data?c=AT&dateFrom=2021-W05&dateTo=2021-W49&range=5"
+      );
+      expect(res.status).toBe(200);
+    });
+  });
 });
